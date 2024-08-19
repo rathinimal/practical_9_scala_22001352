b6bbe6ae6791aebfc948299ef92b22c2e9ebcfeb
rathinimal <rathinarasanimal@gmail.com>
2024-08-19 22:19:23 +0530

    Q1 some changes

--- Q1.scala
+++ Q1.scala
@@ -1,17 +1,18 @@
+import scala.io.StdIn.readDouble
+
 object Q1{
     def main(args:Array[String]): Unit = {
-        val interest : Double => Double = deposit => {
-            deposit match {
-                case d if d <= 20000 => d * 0.02
-                case d if d <= 20000 => d * 0.04
-                case d if d <= 200000 =>d * 0.035
-                case d if d >  2000000 => d * 0.065
-            }
+        val calcInterest = (deposit:Double,rate:Double)=> deposit * rate/100
+        val findInterest = (deposit:Double)=> deposit match {
+            case x if x <= 20000 => calcInterest(x,2)
+            case x if x <= 200000 => calcInterest(x,4)
+            case x if x <= 2000000 => calcInterest(x,3.5)
+            case x => calcInterest(x,6.5)
         }
 
-        val deposit = 40000.0
-        val Interest_for_deposit = interest(deposit)
-        println(s"Intersest earned: Rs.$Interest_for_deposit")
+        print("Enter the deposit amount: ")
+        var depositInput =readDouble()
 
+        println(s"The interest for deposit : ${findInterest(depositInput)}")
     }
 }
